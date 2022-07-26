4b5cac4a26e2052c70d45019670431ada5278c52
Erick Fabiani <erickfabiani123@gmail.com>
2022-07-26 14:14:43 -0300

    Config/sonarcloud ci (#6)
    * SonarCloud configured
    
    * fixed workflow yml
    
    * bla
    
    * fixed the name of application dir
    
    * Exclude js files of analysis
    
    * Added a TODO just for tests in sonarcloud
    
    * Changed the name of app "core" to "model_admin"
    
    * Added .idea to gitignore
    
    * model_admin tests fixed
    
    * Coverage step was add to CI
    
    * Coverage step was add to CI
    
    * Coverage step was add to CI
    
    * KASJDAS
    
    * Changed version of python
    
    * added requirements.txt
    
    * changes main
    
    * changes main
    
    * added coverage path to sonar
    
    * added coverage path to sonar
    
    * added coverage path to sonar
    
    * added coverage path to sonar
    
    * added coverage path to sonar
    
    * added coverage path to sonar
    
    * added coverage path to sonar
    
    * added coverage path to sonar
    
    * added coverage path to sonar
    
    * added coverage path to sonar
    
    * added coverage path to sonar
    
    * added coverage path to sonar
    
    * added coverage path to sonar
    
    * added coverage path to sonar
    
    * added coverage path to sonar
    
    * added coverage path to sonar
    
    * added coverage path to sonar
    
    * added coverage path to sonar
    
    * Added success to sonar
    
    * Changed CI
    
    * Changed CI

---
+++ core/apps/model_admin/hooks/modeladmin/options.py
@@ -0,0 +1,30 @@
+from django.urls import re_path
+from wagtail.contrib.modeladmin.options import ModelAdmin
+
+from apps.model_admin.hooks.buttons.helpers.default import DefaultButtonHelper
+from apps.model_admin.hooks.views.duplicate import DuplicateView
+
+
+class HeroModelAdmin(ModelAdmin):
+
+    button_helper_class = DefaultButtonHelper
+    duplicate_view_class = DuplicateView
+    duplicate_template_name: str = ''
+
+    def duplicate_view(self, request, instance_pk):
+        kwargs = {'model_admin': self, 'instance_pk': instance_pk}
+        view_class = self.duplicate_view_class
+        return view_class.as_view(**kwargs)(request)
+
+    def get_duplicate_template(self):
+        return self.duplicate_template_name or self.get_templates('duplicate')
+
+    def get_admin_urls_for_registration(self):
+        urls = super().get_admin_urls_for_registration()
+        urls = urls + (
+            re_path(
+                self.url_helper.get_action_url_pattern('duplicate'),
+                self.duplicate_view,
+                name=self.url_helper.get_action_url_name('duplicate')),
+        )
+        return urls
---
+++ core/apps/model_admin/tests/hooks/buttons/helpers/test_base.py
@@ -0,0 +1,92 @@
+from unittest.mock import patch, Mock
+
+from django.test import TestCase
+
+from apps.model_admin.hooks.buttons.components import AddButton
+from apps.model_admin.hooks.buttons.helpers.base import BaseButtonHelper
+
+
+class TestBaseHelper(TestCase):
+
+    def setUp(self):
+
+        view = Mock(
+            model=Mock(
+                _meta=Mock(
+                    pk=Mock(attname="id"),
+                    verbose_name="Helper",
+                    verbose_name_plural="Helpers",
+                )
+            ),
+            url_helper=Mock(
+                create_url="add.url"
+            ),
+            permission_helper=Mock()
+        )
+
+        request = Mock()
+        self.c = BaseButtonHelper(view, request)
+        self.c._buttons = [AddButton]
+        self.obj = Mock(id="UUID_QUALQUER")
+
+    def test_get_add_button(self):
+        """Should return a button Rendered"""
+
+        expected = {
+            "url": 'add.url',
+            "label": 'Add Helper',
+            "classname": 'button bicolor icon icon-plus',
+            "title": 'Add a new Helper'
+        }
+
+        self.assertEqual(expected, self.c.get_add_button().to_dict())
+
+    def test_render(self):
+        """Should render a button"""
+        button = AddButton
+        pk = "bla"
+        result = self.c._render(button, pk, [], [])
+        expected = {
+            "url": 'add.url',
+            "label": 'Add Helper',
+            "classname": 'button bicolor icon icon-plus',
+            "title": 'Add a new Helper'
+        }
+
+        self.assertEqual(expected, result)
+
+    def test_return_list_if_value_is_none(self):
+        """Should return a empty list if the value is none"""
+        value = None
+        result = self.c._parse_none(value)
+        expected = []
+
+        self.assertEqual(result, expected)
+
+    def test_return_the_value_if_is_non_empty_list(self):
+        """Should return the list with values"""
+        value = ["teste"]
+        result = self.c._parse_none(value)
+        expected = ["teste"]
+
+        self.assertEqual(expected, result)
+
+    def test_get_pk(self, ):
+        """Must return the correct ID. """
+        result = self.c._get_pk(obj=self.obj)
+        expected = "UUID_QUALQUER"
+
+        self.assertEqual(result, expected)
+
+    def test_get_buttons_for_obj(self):
+        """Should return all buttons as a list of dicts"""
+        result = self.c.get_buttons_for_obj(obj=self.obj)
+        expected = [
+            {
+                "url": 'add.url',
+                "label": 'Add Helper',
+                "classname": 'button bicolor icon icon-plus',
+                "title": 'Add a new Helper'
+            }
+        ]
+        self.assertEqual(result, expected)
