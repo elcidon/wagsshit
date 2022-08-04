1760dd7d346eeb278819f0612522ad8aec8dbcc7
Erick Fabiani <erickfabiani123@gmail.com>
2022-08-04 14:47:20 -0300

    Test/sonarcloud pr (#9)
    * added push event to CI
    
    * Um teste para o vídeo de apresentação

--- core/apps/model_admin/models/common.py
+++ core/apps/model_admin/models/common.py
@@ -4,6 +4,7 @@ from django.db import models
 
 
 class CommonInfo(models.Model):
+    #TODO: Remover esse todo
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField("Criado em", auto_now_add=True)
     updated_at = models.DateTimeField("Modificado em", auto_now=True)
